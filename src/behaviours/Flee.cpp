8758b185d4db3b657782724ff4efd2b48962e5d6
Miron Bury <miron.bury@gmail.com>
2021-06-07 23:07:09 +0100

    revert flee

--- src/behaviours/Flee.cpp
+++ src/behaviours/Flee.cpp
@@ -2,10 +2,20 @@
 #include "PhysicsComponent.h"
 
 Flee::Flee(const TransformComponent* a_pAgent, const glm::vec3* a_pTarget, const PhysicsComponent* a_pPhysicsComponent)
-	: Arrive(a_pAgent, a_pTarget, a_pPhysicsComponent, 0)
+	: Behaviour(a_pAgent, a_pTarget, a_pPhysicsComponent)
 {}
 
-glm::vec3 Flee::Target()
+glm::vec3 Flee::Force()
 {
-	return glm::vec3(AgentPos() - TargetPos());
+	glm::vec3 v3AwayDir(AgentPos() - TargetPos());
+	if (glm::length2(v3AwayDir) == 0)
+	{
+		v3AwayDir = SphericalRand(1); //flee in any direction
+	}
+	else
+	{
+		v3AwayDir = glm::normalize(v3AwayDir);
+	}
+	glm::vec3 v3DesiredVelocity = ClampVelocity(v3AwayDir * m_fSpeed);
+	return ClampForce(v3DesiredVelocity - m_pPhysicsComponent->GetVelocity());
 }
--- src/behaviours/Flee.h
+++ src/behaviours/Flee.h
@@ -1,16 +1,14 @@
 #ifndef FLEE_H
 #define FLEE_H
 
-#include "Arrive.h"
+#include "Behaviour.h"
 
-class Flee : public Arrive
+class Flee : public Behaviour
 {
 public:
     Flee(const TransformComponent* a_pAgent, const glm::vec3* a_pTarget, const PhysicsComponent* a_pPhysicsComponent);
     virtual ~Flee() = default;
-
-private:
-    virtual glm::vec3 Target() override;
+    virtual glm::vec3 Force() override;
 };
 
 #endif //FLEE_H
